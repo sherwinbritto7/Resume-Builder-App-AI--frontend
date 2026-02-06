5d0bcdf014a385d1a69d16c67dbd3191c6e12ad2
Sherwin Britto <sherwinbritto232@gmail.com>
2026-02-06 15:42:02 +0530

    Dynamic welcome message for user in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -130,7 +130,7 @@ const Dashboard = () => {
     <div>
       <div className="max-w-7xl mx-auto px-4 py-8">
         <p className="text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
-          Welcome, John Doe
+          Welcome, {user?.name || "User"}
         </p>
         <div className="flex gap-4">
           <button
